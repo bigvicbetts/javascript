52cafd75f284529c6c7e013fe0f62edb6bbec5e2
Victor Betts <v_betts22@hotmail.com>
2020-05-31 20:02:57 -0500

    Completed Exercise 4 bonus assignment

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,2 +1,43 @@
 console.log("time to start the timer!");
 
+let mins;
+let ones;
+let tens;
+
+function reset() {
+    mins = 0;
+    ones = 0;
+    tens = 0;
+}
+
+function startTimer() {
+    console.log(mins, tens, ones);
+    ones ++;
+    if (ones > 9) {
+        ones = 0;
+        tens ++;
+    }
+
+    if (tens > 5) {
+        tens = 0;
+        mins ++
+    }
+    document.getElementById('digits').textContent = mins + ':' + tens + ones;  
+}
+
+reset();
+
+document.getElementById('start').addEventListener('click', function() {
+    timer = setInterval(startTimer, 1000);
+});
+
+document.getElementById('stop').addEventListener('click', function() {
+    clearInterval(timer);
+    document.getElementById('digits').textContent = mins + ':' + tens + ones;
+});
+
+document.getElementById('reset').addEventListener('click', function () {
+    clearInterval(timer);
+    reset();
+    document.getElementById('digits').textContent = mins + ':' + tens + ones;
+});
